fd5a0c1b3a1b687497b206a5f5076f17eefc5828
Framattat <zinno_98@hotmail.it>
2022-11-19 01:07:53 +0100

    3 files modified

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # progetto-teatro-serviziweb-angular
 
-Chiave partenza: 61bc0196
+Chiave partenza: de3ee7d0
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pxbgrq)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostoComponent } from './posto/posto.component';
+import { NominativoComponent } from './nominativo/nominativo.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostoComponent, NominativoComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
