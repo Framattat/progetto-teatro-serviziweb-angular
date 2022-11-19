9f7c1481c8380ed5170014f5d40eb64c270ea926
Framattat <zinno_98@hotmail.it>
2022-11-19 14:49:00 +0100

    1 file modified

--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable()
-export class DbService {
+//injectable -> quindi sto sviluppando un service
+@Injectable({
+  providedIn: 'root',
+})
+export class dbService {
+  // definisco la URL per la connessione al db
+  URL: string =
+    'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/';
 
-  constructor() { }
+  // rendo disponibile i metodi
+  constructor(private http: HttpClient) {}
 
+  public getDb(key: string): Observable<string> {
+    console.log('la get' + key);
+    //metodo get endpoint get
+    return this.http.get<string>(this.URL + 'get?key=' + key);
+  }
+  public setDb(key: string, body): Observable<string> {
+    console.log('la set' + key);
+    //il metodo della set è post, fornisco anche body
+    return this.http.post<string>(this.URL + 'set?key=' + key, body);
+  }
+  public newDb(): Observable<string> {
+    console.log('la new');
+    //metodo della new è get, con il secret fornito dal professore e l'endpoint new
+    return this.http.get<string>(this.URL + 'new?secret=ssw2022');
+  }
 }
