ef310319f74c1089ebf9277f9b15eabe25fbbaf9
Framattat <zinno_98@hotmail.it>
2022-11-20 17:38:54 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,55 @@
 import { Component } from '@angular/core';
 import { DbService } from './db.service';
 
+interface teatro{
+  platea: number [][],
+  palco: number [][]
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   constructor(private db: DbService) {}
 
-  teatro: string = ' ';
   avviso: string = '';
   stato: number = 0;
+  teatro: teatro = {
+    platea: [[],[]],
+    palco: [[],[]]
+  };
+  costruzione_teatro(
+    f_platea: number = 7,
+    p_platea: number = 10,
+    f_palco: number = 4,
+    p_palco: number = 6
+  ) { 
+      this.teatro.platea = Array(f_platea).fill('') .map(() => Array(p_platea).fill('x'));
+      this.teatro.palco = Array(f_palco).fill('').map(() => Array(p_palco).fill('x'));
+    console.log(this.teatro);
+  }
 
+  //funzione per l'accesso al teatro
   accesso(key: string) {
+    this.costruzione_teatro();
+    return;
+    //controllo se la stringa key (e il relativo campo) è vuota, popolo il campo in caso di campo vuoto
     if (key != '') {
       console.log('Non sono vuoto!');
       this.db.getDb(key).subscribe({
         next: (res: any) => {
           this.avviso = '';
+          // procedo con il prossimo component
           this.stato = 1;
+          // se la chiave non ha un teatro impostato, lo imposto
           if (res == null) {
-            console.log(res);
             this.imposta_teatro(key);
+            this.accesso(key);
           } else {
-            console.log(res);
+            console.log('tutto ok');
           }
         },
         error: (error) => {
@@ -36,15 +61,13 @@ export class AppComponent {
       });
     } else {
       this.avviso = 'Non hai inserito una chiave!';
-      console.log('non hai inserito una chiave');
     }
   }
-
-  imposta_teatro(key: string) {
-    this.db.setDb(key, this.teatro).subscribe({
+  // funzione per impostare il teatro in base al teatro scelto, se il campo non viene specificato si usa il teatro base
+  imposta_teatro(key: string, teatro = this.teatro) {
+    this.db.setDb(key, teatro).subscribe({
       next: () => {
-        this.accesso(key);
-        return;
+        console.log('Teatro impostato');
       },
       error: (error) => {
         console.error(
