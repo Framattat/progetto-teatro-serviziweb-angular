fa9995ea004a588b36c3a16820928aa5bb9fffde
Framattat <zinno_98@hotmail.it>
2022-11-20 15:25:31 +0100

    Modifiche ai template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,33 +9,38 @@ import { DbService } from './db.service';
 export class AppComponent {
   constructor(private db: DbService) {}
 
+  teatro: string = ' ';
+  avviso: string = '';
+
   accesso(key: string) {
     if (key != '') {
       console.log('Non sono vuoto!');
       this.db.getDb(key).subscribe({
         next: (res: any) => {
+          this.avviso = '';
           if (res == null) {
             console.log(res);
             this.imposta_teatro(key);
-            return;
           } else {
             console.log(res);
           }
         },
         error: (error) => {
+          this.avviso = 'Sembra che la chiave "' + key + '" non esista';
           console.error(
             'Accesso ha generato un errore: ' + JSON.stringify(error)
           );
         },
       });
     } else {
+      this.avviso = 'Non hai inserito una chiave!';
       console.log('non hai inserito una chiave');
     }
   }
 
   imposta_teatro(key: string) {
-    this.db.setDb(key, 'Ora sono settato').subscribe({
-      next: (res: any) => {
+    this.db.setDb(key, this.teatro).subscribe({
+      next: () => {
         this.accesso(key);
         return;
       },
