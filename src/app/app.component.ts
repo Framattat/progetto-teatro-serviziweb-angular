2a8ca5f502b2c30dad5ba52bc1f471c51730e3bd
Framattat <zinno_98@hotmail.it>
2022-11-21 11:56:16 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,8 @@ export class AppComponent {
   constructor(private db: DbService) {}
 
   avviso: string = '';
-  chiave: string ="";
+  key: string = '';
   stato: number = 0;
-  teatro_export: any = "";
-  teatro: any = {
-    platea: [[], []],
-    palco: [[], []],
-  };
-
-  costruzione_teatro(
-    f_platea: number = 7,
-    p_platea: number = 10,
-    f_palco: number = 4,
-    p_palco: number = 6
-  ) {
-    this.teatro.platea = Array(f_platea)
-      .fill('')
-      .map(() => Array(p_platea).fill('x'));
-    this.teatro.palco = Array(f_palco)
-      .fill('')
-      .map(() => Array(p_palco).fill('x'));
-    console.log(this.teatro);
-  }
 
   //funzione per l'accesso al teatro
   accesso(key: string) {
@@ -40,22 +20,12 @@ export class AppComponent {
       console.log('Non sono vuoto!');
       this.db.getDb(key).subscribe({
         next: (res: any) => {
+          // passo la key al prossimo component
+          this.key = key;
+          // resetto l'avviso
           this.avviso = '';
           // procedo con il prossimo component
           this.stato = 1;
-          
-          // se la chiave non ha un teatro impostato, lo imposto
-          if (res == null) {
-            this.teatro = this.costruzione_teatro();
-            this.imposta_teatro(key, this.teatro);
-            this.accesso(key);
-          } else {
-            this.chiave = key;
-            this.teatro_export = res;
-            console.log('io sono res: ' + this.teatro_export);
-            console.log('io sono il teatro: ' + this.teatro);
-            console.log('tutto ok');
-          }
         },
         error: (error) => {
           this.avviso = 'Sembra che la chiave "' + key + '" non esista';
@@ -67,18 +37,5 @@ export class AppComponent {
     } else {
       this.avviso = 'Non hai inserito una chiave!';
     }
-  }
-  // funzione per impostare il teatro in base al teatro scelto, se il campo non viene specificato si usa il teatro base
-  imposta_teatro(key: string, teatro) {
-    this.db.setDb(key, teatro).subscribe({
-      next: () => {
-        console.log('Teatro impostato');
-      },
-      error: (error) => {
-        console.error(
-          'Imposta_teatro ha generato un errore: ' + JSON.stringify(error)
-        );
-      },
-    });
   }
 }
--- src/app/nominativo/nominativo.component.ts
+++ src/app/nominativo/nominativo.component.ts
@@ -1,15 +1,76 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { DbService } from '../db.service';
 @Component({
   selector: 'app-nominativo',
   templateUrl: './nominativo.component.html',
   styleUrls: ['./nominativo.component.css'],
 })
 export class NominativoComponent implements OnInit {
+  constructor(private db: DbService) {}
+  @Input() key: string;
+  avviso: string;
+  stato: number;
+  teatro_export: string;
 
-  constructor() {
+  teatro: any = {
+    platea: [[], []],
+    palco: [[], []],
+  };
+
+  costruzione_teatro(
+    f_platea: number = 7,
+    p_platea: number = 10,
+    f_palco: number = 4,
+    p_palco: number = 6
+  ) {
+    this.teatro.platea = Array(f_platea)
+      .fill('')
+      .map(() => Array(p_platea).fill('x'));
+    this.teatro.palco = Array(f_palco)
+      .fill('')
+      .map(() => Array(p_palco).fill('x'));
+    console.log(this.teatro);
   }
 
+  //funzione per l'accesso al teatro
+  controllo_accesso() {
+      this.db.getDb(this.key).subscribe({
+        next: (res: any) => {
+          // se la chiave non ha un teatro impostato, lo imposto
+          if (res == null) {
+            this.teatro = this.costruzione_teatro();
+            this.imposta_teatro(this.key, this.teatro);
+            this.controllo_accesso();
+          } else {
+            this.teatro_export = res;
+            console.log('io sono res: ' + this.teatro_export);
+            console.log('io sono il teatro: ' + this.teatro);
+          }
+          // resetto l'avviso
+          this.avviso = '';
+          // procedo con il prossimo component
+          this.stato = 2;
+        },
+        error: (error) => {
+          console.error(
+            'Controllo accesso ha generato un errore: ' + JSON.stringify(error)
+          );
+        },
+      });
+  }
+  // funzione per impostare il teatro in base al teatro scelto, se il campo non viene specificato si usa il teatro base
+  imposta_teatro(key: string, teatro) {
+    this.db.setDb(key, teatro).subscribe({
+      next: () => {
+        console.log('Teatro impostato');
+      },
+      error: (error) => {
+        console.error(
+          'Imposta_teatro ha generato un errore: ' + JSON.stringify(error)
+        );
+      },
+    });
+  }
 
   ngOnInit() {}
 }
