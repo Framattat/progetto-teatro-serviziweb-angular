4c228b1501777263d02148d0ee474f4e88ecbf15
Framattat <zinno_98@hotmail.it>
2022-11-23 14:45:58 +0100

    Pulito codice su posto.component

--- src/app/posto/posto.component.ts
+++ src/app/posto/posto.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DbService } from '../db.service';
+import { NominativoComponent } from '../nominativo/nominativo.component';
 
 @Component({
   selector: 'app-posto',
@@ -11,59 +12,34 @@ export class PostoComponent implements OnInit {
   @Input() nominativo_teatro: string;
   @Input() key: string;
   teatro: any;
-  platea: any;
-  palco: any;
   posto: string = '';
   stato: number = 2;
   avviso: string = '';
   constructor(private db: DbService) {}
 
   prenotazione_posto(fila: number, posto: number, zona: string) {
-    switch (zona) {
-      case 'platea': {
-        if (this.teatro.platea[fila][posto] == 'x') {
-          this.teatro.platea[fila][posto] = this.nominativo_teatro;
-        } else {
-          this.avviso = 'Posto occupato da: ' + this.teatro.platea[fila][posto];
-          return;
-        }
-        break;
+    if (zona == 'platea') {
+      if (this.teatro.platea[fila][posto] == 'x') {
+        this.teatro.platea[fila][posto] = this.nominativo_teatro;
+      } else {
+        this.avviso = 'Posto occupato da: ' + this.teatro.platea[fila][posto];
+        return;
       }
-      case 'palco': {
-        if (this.teatro.palco[fila][posto] == 'x') {
-          this.teatro.palco[fila][posto] = this.nominativo_teatro;
-        } else {
-          this.avviso = 'Posto occupato da: ' + this.teatro.palco[fila][posto];
-          return;
-        }
-        break;
+    } else {
+      if (this.teatro.palco[fila][posto] == 'x') {
+        this.teatro.palco[fila][posto] = this.nominativo_teatro;
+      } else {
+        this.avviso = 'Posto occupato da: ' + this.teatro.palco[fila][posto];
+        return;
       }
-      default:
-        this.avviso = 'Non stai scegliendo ne la platea ne il palco!';
     }
     this.posto = 'nella zona ' + zona + ' n. P' + (fila + 1) + posto;
-    this.aggiorna_teatro(this.teatro);
+    var teatro_aggiornato = new NominativoComponent(this.db);
+    teatro_aggiornato.imposta_teatro(this.key, this.teatro);
     this.stato = 1;
     this.avviso = '';
   }
-
-  // funzione per impostare il teatro in base al teatro scelto, se il campo non viene specificato si usa il teatro base
-  aggiorna_teatro(teatro) {
-    this.db.setDb(this.key, teatro).subscribe({
-      next: () => {
-        console.log('Teatro impostato');
-      },
-      error: (error) => {
-        console.error(
-          'Imposta_teatro ha generato un errore: ' + JSON.stringify(error)
-        );
-      },
-    });
-  }
-
   ngOnInit() {
     this.teatro = JSON.parse(this.teatro_export);
-    this.platea = this.teatro.platea;
-    this.palco = this.teatro.palco;
   }
 }
